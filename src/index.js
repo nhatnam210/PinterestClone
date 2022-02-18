26a9fe6651767ae7b7af313dd4d92bfac1f6b9f0
Trần  Nguyễn Nhật Nam <tnnhatnam210@gmail.com>
2022-02-18 15:43:41 +0700

    test format code husky

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-                                  res.send('Hello World!');
+                                          res.send('Hello World!');
 });
 
 app.listen(port,                () => {
     console.log(
-                                 `Ctrl + click this link to open your website... http://localhost:${port}`,
+                                         `Ctrl + click this link to open your website... http://localhost:${port}`,
     );
 });
