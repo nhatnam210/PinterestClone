298f99c805b65e08a8d912772686394ff1b54130
Trần  Nguyễn Nhật Nam <tnnhatnam210@gmail.com>
2022-02-20 18:34:35 +0700

    login page

---
+++ template/js/login.js
@@ -0,0 +1,16 @@
+// $('.grid')
+// $('.heading .wrapper .text')
+
+function enterScreen(index) {}
+
+function exitScreen(index, exitDelay) {}
+function setupAnimationCycle({
+    initialScreenIndex,
+    timePerScreen,
+    exitDelay,
+}) {}
+setupAnimationCycle({
+    initialScreenIndex: 0,
+    timePerScreen: 2000, //ms
+    exitDelay: 200 * 7, //ms
+});
